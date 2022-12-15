60ad1c1f45b03405512720fdfdb929328fae505e
ali chalki <alichalki@outlook.com>
2022-12-16 00:07:44 +0330

    initController

--- ApiDocumentationUsingRedocAndSwagger/Controllers/UserController.cs
+++ ApiDocumentationUsingRedocAndSwagger/Controllers/UserController.cs
@@ -10,14 +10,32 @@ namespace ApiDocumentationUsingRedocAndSwagger.Controllers
         [HttpPost(Name = "CreateUser")]
         public IActionResult Create(UserAccount inputdata)
         {
+
+            SampleData.Users.Add(inputdata);
             return Ok();
         }
 
 
         [HttpGet(Name = "GetUser")]
-        public IActionResult Get(UserAccount inputdata)
+        public IActionResult Get(string firstname)
         {
-            return Ok(inputdata);
+            var res = SampleData.Users.FirstOrDefault(e => e.Firstname.Equals(firstname));
+            if (res == null)
+                return Ok("کاربری با این مشخصات یافت نشد");
+
+            return Ok(res);
+        }
+
+        [HttpDelete(Name = "DeleteUser")]
+        public IActionResult Delete(string firstname)
+        {
+            var res = SampleData.Users.FirstOrDefault(e => e.Firstname.Equals(firstname));
+            SampleData.Users.Remove(res);
+
+            if (res == null)
+                return Ok("کاربری با این مشخصات یافت نشد");
+
+            return Ok(res);
         }
     }
 }
---
+++ ApiDocumentationUsingRedocAndSwagger/SampleData.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiDocumentationUsingRedocAndSwagger
+{
+    public static class SampleData
+    {
+        static SampleData() => Users = new List<UserAccount>();
+
+        public static List<UserAccount> Users { get; set; }
+    }
+}
