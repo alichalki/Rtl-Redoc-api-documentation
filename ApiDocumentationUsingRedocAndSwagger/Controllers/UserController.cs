59df54083d1d44f8890dd2e52b8269d2276bbd64
ali chalki <alichalki@outlook.com>
2022-12-15 23:37:29 +0330

    Add project files.

---
+++ ApiDocumentationUsingRedocAndSwagger/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDocumentationUsingRedocAndSwagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost(Name = "CreateUser")]
+        public IActionResult Create(UserAccount inputdata)
+        {
+            return Ok();
+        }
+
+
+        [HttpGet(Name = "GetUser")]
+        public IActionResult Get(UserAccount inputdata)
+        {
+            return Ok(inputdata);
+        }
+    }
+}
